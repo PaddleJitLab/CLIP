5b7ce1a40f6bdefdfa9a78ddfa7c6421f7ed56f2
SigureMo <sigure.qaq@gmail.com>
2025-10-16 21:00:06 +0800

    add missing text input

--- export.py
+++ export.py
@@ -3,12 +3,12 @@ import clip
 
 if __name__ == "__main__":
     x = torch.randn(1, 3, 224, 224)
-    # model, preprocess = clip.load("ViT-B/32", device=torch.cpu)
+    text_input = torch.randn(3, 77)
     model = clip.model.CLIP(
         512, 224, 12, 768, 32, 77, 49408, 512, 8, 12
     )
     try:
-        torch.export.export(model, (x,))
+        torch.export.export(model, (x, text_input))
         print("[JIT] torch.export successed.")
         exit(0)
     except Exception as e:
